67956b173c7c94f479ea826bf76cf6729083f504
Cong Dat <datnc@leaftech.vn>
2020-05-14 14:03:35 +0700

    Develop | Define base structure | Implement Side Navigation bar | Add flex-layout

--- main-website/src/app/shared/shared.module.ts
+++ main-website/src/app/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { MaterialModule } from './material/material.module';
 import { HeaderComponent } from './components/header/header.component';
@@ -9,6 +10,7 @@ const directives = [
 ];
 const modulesExport = [
   CommonModule,
+  FlexLayoutModule,
   MaterialModule,
 ];
 const componentsExport = [
@@ -20,12 +22,13 @@ const pipes = [
 
 @NgModule({
   declarations: [
-    directives, 
-    pipes, 
+    directives,
+    pipes,
     HeaderComponent,
   ],
   imports: [
     CommonModule,
+    FlexLayoutModule,
     RouterModule,
     MaterialModule
   ],
